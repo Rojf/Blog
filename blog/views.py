71b6ed16b53bb39fd2a10af31018a5479e04f3cd
Blank <rojermen96@gmail.com>
2024-04-25 06:41:22 -0700

    update blog_Post_model, blog_views_post-detail, blog_urls (Creating search engine optimization-friendly post URLs)

--- blog/views.py
+++ blog/views.py
@@ -5,11 +5,16 @@ from .models import Post
 
 def post_list(request):
     posts = Post.published.all()
-    print(posts[0].get_absolute_url)
+
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, id=pk, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})

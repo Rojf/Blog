8186315a90c2bbc555d834b6c9e3b1b87e8387e2
Blank <rojermen96@gmail.com>
2024-04-25 08:13:02 -0700

    update_blog_view -Changing class-based views from ListView to View.

--- blog/views.py
+++ blog/views.py
@@ -1,31 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.views.generic import ListView
+from django.views import View
 
 from .models import Post
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+class PostListView(View):
+    def get(self, request, *args, **kwargs):
+        post_list = Post.published.all()
 
+        paginator = Paginator(post_list, 10)
+        page_number = request.Get.get('page', 1)
 
-def post_list(request):
-    post_list = Post.published.all()
+        try:
+            posts = paginator.page(page_number)
+        except PageNotAnInteger:
+            posts = paginator.page(1)
+        except EmptyPage:
+            posts = paginator.page(paginator.num_pages)
 
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page', 1)
-
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
-
-    return render(request, 'blog/post/list.html', {'posts': posts})
+        return render(request, "blog/post/list.html", {'posts': posts})
 
 
 def post_detail(request, year, month, day, post):

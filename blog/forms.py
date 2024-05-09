6e03c8b8a232fa561531aef43ffd522adbb4656b
Blank <rojermen96@gmail.com>
2024-05-09 13:45:43 -0700

    delete comment from blog/forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,23 +1,14 @@
 from django import forms
 
 from blog.repositories import Repository
-from blog.models import Comment
-
-
-class _CommentForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        fields = ['body', 'author']
 
 
 class CommentForm(forms.Form):
     body = forms.CharField()
-    # author = forms.ModelChoiceField(queryset=get_user())
     author = forms.ModelChoiceField(queryset=Repository.UserRepository.all())
 
 
 class EmailPostForm(forms.Form):
-    # author = forms.ModelChoiceField(queryset=get_user())
     author = forms.ModelChoiceField(queryset=Repository.UserRepository.all())
     email = forms.EmailField()
     to = forms.EmailField()
